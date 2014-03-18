cd0672e73b9776c737deea647c03602ad2e3a4c3
Matt Relich <mrelich6@gmail.com>
2014-03-18 10:49:38 +0900

    Adding skeleton classes and dir structure

---
+++ include/Dim1.h
@@ -0,0 +1,24 @@
+#ifndef Dim1_h
+#define Dim1_h
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+// This class will have methods to perform the parameterized  //
+// 1-D calculation of the electric field given a distribution //
+// of the charge excess as a function of distance.            //
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+
+#include "TMath.h"
+
+class Dim1 
+{
+  
+ public:
+  
+  // Constructor/Destructor
+  Dim1();
+  virtual ~Dim11();  
+
+
+}
+
+#endif
---
+++ include/Physics.h
@@ -0,0 +1,19 @@
+#ifndef Physics_h
+#define Physics_h
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+// Class to house the physics constants needed to calculate //
+// the electric field.                                      //
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+
+namespace Phys
+{
+  
+  // Fundamental Constants
+  
+  // Constants for simulation
+
+
+};
+
+#endif
